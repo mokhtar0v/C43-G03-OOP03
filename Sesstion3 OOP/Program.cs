483e249ef0066ac0f61b2b3da2294df85595d7f1
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-01-06 17:58:37 +0200

    Function Overloading

--- Sesstion3 OOP/Program.cs
+++ Sesstion3 OOP/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Sesstion3_OOP
+﻿using System.ComponentModel;
+
+namespace Sesstion3_OOP
 {
     internal class Program
     {
+        static int sumnum(int x, int y)
+        {
+            return x + y;
+        }
+        static double sumnum(double x, double y)
+        {
+            return x + y;
+        }
+        static int sumnum(int x,int y,int z)
+        {
+            return x + y + z;
+        }
         static void Main(string[] args)
         {
-            
+            #region Overloading
+            dynamic obj = sumnum(1, 2);
+            Console.WriteLine(obj);
+            #endregion
         }
     }
 }
